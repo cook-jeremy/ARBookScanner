2c30a29deba0627382028b25816c27e8b454d506
Jeremy Cook <jeremycook97@gmail.com>
2023-04-10 21:59:03 -0500

    first commit

--- FirstARApp/ContentView.swift
+++ FirstARApp/ContentView.swift
@@ -7,37 +7,44 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
-    }
+   var body: some View {
+      ARViewContainer().edgesIgnoringSafeArea(.all)
+   }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
-    func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
-        return arView
-        
-    }
-    
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+   
+   var bookScene: Experience.BookScene
+   
+   init() {
+      bookScene = try! Experience.loadBookScene()
+   }
+   
+   func makeUIView(context: Context) -> ARView {
+      
+      let arView = ARView(frame: .zero)
+      arView.debugOptions = ARView.DebugOptions.showAnchorOrigins
+
+      bookScene.bookDescription?.isEnabled = false
+      
+      // Add the box anchor to the scene
+      arView.scene.anchors.append(bookScene)
+      return arView
+   }
+   
+   func updateUIView(_ uiView: ARView, context: Context) {
+      
+   }
+   
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
 #endif
